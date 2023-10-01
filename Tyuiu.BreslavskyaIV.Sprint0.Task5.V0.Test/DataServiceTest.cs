a89e39afcb1c614c4f6bf12a40494dc6eb8f6f9d
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-10-01 23:24:13 +0500

    Add files via upload

---
+++ Tyuiu.BreslavskyaIV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BreslavskyaIV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+
+        }
+
+        [TestMethod]
+        public void ChekedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+
+        }
+
+        [TestMethod]
+        public void ChekedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+
+        }
+
+        [TestMethod]
+        public void ChekedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+
+        }
+    }
+}
---
+++ Tyuiu.BreslavskyaIV.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BreslavskyaIV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BreslavskyaIV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("026c46b3-06fb-4426-8e87-3084ecf84a4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
