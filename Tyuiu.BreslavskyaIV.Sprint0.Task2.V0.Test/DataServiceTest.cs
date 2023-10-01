c21337b4fd483217b111e3b3458158fdcef2de4b
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-10-01 23:15:24 +0500

    Add files via upload

---
+++ Tyuiu.BreslavskyaIV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BreslavskyaIV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Ира";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Ира", res);
+        }
+    }
+}
---
+++ Tyuiu.BreslavskyaIV.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BreslavskyaIV.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BreslavskyaIV.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad7d4df6-6594-4062-969e-879a14af6883")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
